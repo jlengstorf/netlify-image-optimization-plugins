8ca18fe55f15f127efcd016a806303f284a072f5
Jason Lengstorf <jason@lengstorf.com>
2019-12-13 17:18:14 -0800

    wip: kinda sorta doing stuff

--- packages/netlify-plugin-use-cloudinary/README.md
+++ packages/netlify-plugin-use-cloudinary/README.md
@@ -1,3 +1,7 @@
 # netlify-plugin-use-cloudinary
 
 Image optimization is slow and difficult. This plugin sends off local images from your site to Cloudinary and replaces references to them with Cloudinary asset URLs, which speeds up builds, reduces your bandwidth usage, and gives you all the superpowers that come along with using Cloudinary for image asset delivery.
+
+## Usage
+
+
--- packages/netlify-plugin-use-cloudinary/index.js
+++ packages/netlify-plugin-use-cloudinary/index.js
@@ -1,11 +1,41 @@
-const DEFAULT_CONFIG = {
-  cloudinaryBaseURL: 'https://res.cloudinary.com',
-  transformations: 'f_auto,q_auto',
+const withDefaults = config =>
+  Object.assign(
+    {
+      cloudinaryBaseURL: 'https://res.cloudinary.com',
+      transformations: 'f_auto,q_auto',
+      srcDirectory: 'src',
+    },
+    config,
+  );
+
+const fs = require('fs');
+const path = require('path');
+const useCloudinary = require('remark-local-image-to-cloudinary');
+const { parseMDX, stringifyMDX } = require('@blocks/serializer');
+
+const astSmasher = async filepath => {
+  // TODO port this over so we can add the cloudinary plugin
+  // https://github.com/blocks/blocks/blob/4fa5593bb4b7c95d7dae928372492860b69706a1/packages/mdx/serializer/src/index.js#L13-L33
+  const contents = await parseMDX(fs.readFileSync(filepath));
+
+  console.log(contents);
+
+  fs.writeFileSync(
+    `${process.cwd()}/processed/updated.mdx`,
+    stringifyMDX(contents),
+  );
 };
 
+const glob = require('glob');
 module.exports = () => ({
-  name: 'gatsby-plugin-use-cloudinary',
-  onPreBuild: async args => {
-    console.log({ args });
+  name: 'netlify-plugin-use-cloudinary',
+  onPreBuild: async ({ pluginConfig }) => {
+    const { srcDirectory } = withDefaults(pluginConfig);
+
+    const files = glob.sync(`${srcDirectory}/**/*.mdx`);
+
+    const promises = files.map(file => astSmasher(file));
+
+    return await Promise.all(promises);
   },
 });
